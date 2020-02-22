0b75cad6d9402dfcacd5d54485d24a7f0b41831f
Matteo Bante <mbante@c2r16s1.42madrid.com>
2020-02-22 19:48:29 +0100

    matteo.c updated first line in destination var

--- ex00/matteo.c
+++ ex00/matteo.c
@@ -13,6 +13,7 @@ int main(int argc, char **argv)
 	char *c;
 	int n;
 	int count;
+	char *destination;
 
 	struct rush_dict
 	{
@@ -20,8 +21,9 @@ int main(int argc, char **argv)
 		char *val;
 	};
 
+	destination = malloc(100);
 	struct rush_dict *tab;
-	tab = (struct rush_dict *) malloc(sizeof(struct rush_dict)*10);
+	tab = (struct rush_dict *) malloc(sizeof(struct rush_dict));
 	count = 0;
 	c = (char *) malloc(sizeof(char) * 100);
 	n = 0;
@@ -30,45 +32,20 @@ int main(int argc, char **argv)
 	fd = open (PATH, O_RDONLY);
 	sz = read(fd, c, 100);
 	
-	/*
-	tab[0].num = "Hola";
-	printf("tab0 %s\n", tab[0].num);
-	tab[0].num = "Hala";
-	printf("tab0 %s\n", tab[0].num);
-	*/
-
-	// while (c[count] != '\n')
-	// {
-	// 	tab[0].num[count] = 'a';
-	// 	count++; 
-	// }
-	//tab[0].num[40];
-	ft_strncpy(tab[0].num, c, 2);
-	printf("tab0 %s\n", tab[0].num);
-
-
-
-	//printf("fd = %d\n", fd);
-	//printf("called read(%d, c, 100).  returned that %d bytes  were read.\n", fd, sz); 
 	c[sz] = '\0';
 
 	read_args(argc, argv);
-	write(1, "\n", 1);
-
-	printf("strcmp= %d\n", ft_strcmp(c, argv[1]));
-	//printf("Those bytes are as follows: \n%s\n", c);
 	while (c[n] != 10)
 	{
-		if (c[n] != 58 && n < 10)
-		{
-			count++;
-			break ;
-		}
 		n++;
 	}
-	printf("%d\n", count);
-	//tab[0].num = ft_strncpy(tab[0].num, c, count);
-	//printf("%s\n", tab[0].num);
+	ft_strncpy(destination, c, n);
+	destination[n] = '\0';
+	printf("destintation=  %s\n", destination);
+	while (c[n] != 10)
+	{
+		n++;
+	}
 	close(fd);
 	return (0);
 }
