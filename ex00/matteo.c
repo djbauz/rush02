9fa63a6962f04e1ff87dcb07ad33088c1fde9134
Matteo Bante <mbante@c2r16s1.42madrid.com>
2020-02-22 19:00:54 +0100

    try to put first line of dict in a char* variable

--- ex00/matteo.c
+++ ex00/matteo.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "read_args.c"
+#include "ft_strcmp.c"
+#include "ft_strncpy.c"
 #define PATH "numbers.dict"
 
 int main(int argc, char **argv)
@@ -14,30 +16,34 @@ int main(int argc, char **argv)
 
 	struct rush_dict
 	{
-		char num[];
-		char val[];
+		char *num;
+		char *val;
 	};
 
 	struct rush_dict *tab;
-	tab = (struct rush_dict *) malloc(sizeof(struct rush_dict));
+	tab = (struct rush_dict *) malloc(sizeof(struct rush_dict)*10);
 	count = 0;
 	c = (char *) malloc(sizeof(char) * 100);
-	n = 1;
+	n = 0;
 	printf("argc= %d\n", argc);
 	printf("argv= %s\n", argv[1]);
 	fd = open (PATH, O_RDONLY);
 	sz = read(fd, c, 100);
 	
+	/*
 	tab[0].num = "Hola";
 	printf("tab0 %s\n", tab[0].num);
 	tab[0].num = "Hala";
 	printf("tab0 %s\n", tab[0].num);
+	*/
 
 	// while (c[count] != '\n')
 	// {
 	// 	tab[0].num[count] = 'a';
 	// 	count++; 
 	// }
+	//tab[0].num[40];
+	ft_strncpy(tab[0].num, c, 2);
 	printf("tab0 %s\n", tab[0].num);
 
 
@@ -47,17 +53,22 @@ int main(int argc, char **argv)
 	c[sz] = '\0';
 
 	read_args(argc, argv);
+	write(1, "\n", 1);
 
+	printf("strcmp= %d\n", ft_strcmp(c, argv[1]));
 	//printf("Those bytes are as follows: \n%s\n", c);
-	while (c[n] != '\0')
+	while (c[n] != 10)
 	{
-		if(c[n] == argv[1][0])
+		if (c[n] != 58 && n < 10)
 		{
 			count++;
-			printf("%c %d\n", c[n], count);
+			break ;
 		}
 		n++;
 	}
+	printf("%d\n", count);
+	//tab[0].num = ft_strncpy(tab[0].num, c, count);
+	//printf("%s\n", tab[0].num);
 	close(fd);
 	return (0);
 }
