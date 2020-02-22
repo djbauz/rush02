20b7b6a926014e63dd995fe2e20ba4e7b0c8e75c
Miguel Ángel Adorna Ruiz <madorna-@student.42madrid.com>
2020-02-22 17:23:01 +0100

    Added one_arg

--- ex00/read_args.c
+++ ex00/read_args.c
@@ -6,24 +6,38 @@
 /*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 16:35:59 by madorna-          #+#    #+#             */
-/*   Updated: 2020/02/22 16:57:59 by madorna-         ###   ########.fr       */
+/*   Updated: 2020/02/22 17:21:45 by madorna-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*read_args(int count, char **args)
+int		one_arg(char *args);
+
+void	read_args(int count, char **args)
 {
 	if (count != 2 && count != 3)
 	{
 		write(1, "Error\n", 6);
+		return ;
 	}
 	else if (count == 2)
 	{
 		write(1, "1 param", 8);
+		one_arg(args[1]);
 	}
 	else if (count == 3)
 	{
 		write(1, "2 params", 9);
 	}
-	
-		
+}
+
+int		one_arg(char *args)
+{
+	int i;
+
+	i = 0;
+	while (args[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
