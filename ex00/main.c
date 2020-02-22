630510763dfddf32dc12c9dcdf03ac8b5807a72b
Miguel Ángel Adorna Ruiz <madorna-@student.42madrid.com>
2020-02-22 22:40:57 +0100

    Added changes

---
+++ ex00/main.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   matteo.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/22 20:30:58 by madorna-          #+#    #+#             */
+/*   Updated: 2020/02/22 20:50:17 by madorna-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h> // open and create
+#include <unistd.h> // close
+#include <stdio.h>
+#include <stdlib.h>
+#include "read_args.c"
+#include "ft_strcmp.c"
+#include "ft_strncpy.c"
+#define PATH "numbers.dict"
+
+int main(int argc, char **argv)
+{
+	int fd;
+	int sz;
+	char *c;
+	int n;
+	int count;
+	char *destination;
+	int helper;
+
+	struct rush_dict
+	{
+		char *num;
+		char *val;
+	};
+
+	read_args(argc, argv);
+	destination = malloc(100);
+	struct rush_dict *tab;
+	tab = (struct rush_dict *) malloc(sizeof(struct rush_dict));
+	count = 0;
+	c = (char *) malloc(sizeof(char) * 100);
+	n = 0;
+	printf("argc= %d\n", argc);
+	printf("argv= %s\n", argv[1]);
+	fd = open (PATH, O_RDONLY);
+	sz = read(fd, c, 100);
+	
+	c[sz] = '\0';
+
+	helper = 0;
+	while (c[n] != '\0')
+	{
+		if(c[n] )
+		n++;
+	}
+	ft_strncpy(destination, c, n);
+	destination[n] = '\0';
+	printf("destintation=  %s\n", destination);
+	tab[0].num = destination;
+	close(fd);
+	return (0);
+}
